bbb5c5dc49f08a61059adea23fdea59ad77b745b
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 21:36:21 -0500

    logica final

--- corte2/logica.js
+++ corte2/logica.js
@@ -3,6 +3,8 @@ function validar(){
     let checkboxq = document.getElementById("q").checked;
     let resultado = "p AND q es :" + (checkboxp&&checkboxq)+"<br>";
     resultado=resultado+"p OR q es :"+ (checkboxp||checkboxq)+"<br>";
+    resultado=resultado+"la negacion de P es "+(!checkboxp)+"<br>";
+    resultado=resultado+"la negacion de Q es "+(!checkboxq)+"<br>";
     
 
 
