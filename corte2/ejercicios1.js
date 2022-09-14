77faedd12b3178f94ad1a3e94c4745623a9329c9
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 21:08:07 -0500

    logica

--- corte2/ejercicios1.js
+++ corte2/ejercicios1.js
@@ -1,3 +1,4 @@
 let grados = prompt("cual es la temperatura de ci ciudad en grados centigrados ?");
-let gradosconv = (grados*1.8)+32
-alert("la temperatura de su ciudad en grados Fahrenheit es "+ gradosconv)
+let gradosF = (grados*1.8)+32;
+let gradosk = Number (grados)+273.15;
+alert("la temperatura de su ciudad en grados Fahrenheit es "+ gradosF + " y en grados Kelvin es " + gradosk);
---
+++ corte2/logica.js
@@ -0,0 +1,8 @@
+function validar(){
+    let checkboxp = document.getElementById("p").checked;
+    let checkboxq = document.getElementById("q").checked;
+
+    console.log(checkboxp);
+    console.log(checkboxq);
+
+}
