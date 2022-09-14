44936815c91680e15cce1fc3cf78b34d59795967
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 21:29:26 -0500

    LOGICA

--- corte2/logica.js
+++ corte2/logica.js
@@ -1,8 +1,14 @@
 function validar(){
     let checkboxp = document.getElementById("p").checked;
     let checkboxq = document.getElementById("q").checked;
+    let resultado = "p AND q es :" + (checkboxp&&checkboxq)+"<br>";
+    resultado=resultado+"p OR q es :"+ (checkboxp||checkboxq)+"<br>";
+    
+
 
     console.log(checkboxp);
     console.log(checkboxq);
-
+    document.getElementById("mensaje").innerHTML=resultado;
+    
 }
+
