cb000cbd0843681256dd683a8daa87d8311d512a
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:31:11 -0500

    2corte

---
+++ corte2/corte2.js
@@ -0,0 +1,14 @@
+// alert("hola");
+document.write("Escuela de artes y letras");
+console.log("Martes 13 ");
+ let name = prompt("porfavor escriba su nombre");
+let nombre = name;
+document.getElementById("cuadro1").innerHTML=nombre;
+ alert("Hello there "+ name);
+ let age = prompt("escriba su edad") ;
+ let edadfutura = Number(age)+10;
+ let edadanterior =Number(age)-5;
+ let edadtriple =Number(age)*3
+ let edadmitad =Number(age)/2
+ alert(name + " tu edad en 10 años sera  " +edadfutura + ", hace 5 años era " +edadanterior + ", el triple es "+ edadtriple +" y la mitad es "+edadmitad );
+ 
