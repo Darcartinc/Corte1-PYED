091808f1ae8b0c57b2834ec4dcdd1885b1f98a16
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-20 20:44:36 -0500

    futbol

---
+++ corte2/class7.js
@@ -0,0 +1,77 @@
+let animales = ["leon","salmon","mirla","gato","zorro"];
+let pais ={
+    "nombre": "Canada",
+    "capital":"Ottawa",
+    "kilometros":"9.985M km",
+    "idioma":"Frances e ingles",
+    "PIB":"1,643 billones USD"
+
+};
+let cantantes = [
+    {
+    "nombre": "Billie_Eilish ",
+    "nacionalidad":"Estados_Unidos",
+    "genero_musical":"Alternativo/Independiente",
+
+    "canciones":[
+        {
+            "nombre":"Bad_Guy",
+            "reproducciones":"20"
+        },
+        {
+            "nombre":"Bury_a_friend",
+            "reproducciones":"15"
+        },
+        {
+            "nombre":"My_strange_addiction",
+            "reproducciones":"16"
+        },
+    ]
+    },
+    {
+        "nombre": "Michael_Jackson ",
+        "nacionalidad":"Estados_Unidos",
+        "genero_musical":"Pop/rock",
+    
+        "canciones":[
+            {
+                "nombre":"Billie_jean",
+                "reproducciones":"27"
+            },
+            {
+                "nombre":"dangerous",
+                "reproducciones":"19"
+            },
+            {
+                "nombre":"smooth_criminal",
+                "reproducciones":"26"
+            },
+        ]
+        },
+        {
+            "nombre": "QUEEN ",
+            "nacionalidad":"Inglaterra",
+            "genero_musical":"rock/R&B",
+        
+            "canciones":[
+                {
+                    "nombre":"Show must go on",
+                    "reproducciones":"21"
+                },
+                {
+                    "nombre":"Killer queen",
+                    "reproducciones":"29"
+                },
+                {
+                    "nombre":"Bohemian raphsodi",
+                    "reproducciones":"22"
+                },
+            ]
+            }
+
+
+];
+
+
+
+console.log(cantantes);
