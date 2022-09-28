3871f40be5f364d74c9775f2c401f584c45de729
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-27 20:28:40 -0500

    yes

--- corte2/Clases/funciones.js
+++ corte2/Clases/funciones.js
@@ -3,10 +3,17 @@ function convertirTexto()
   let texto=document.querySelector("#texto").value ;
   let contenedor=document.querySelector("#contenedor");
   contenedor.innerHTML=mayusculas(texto);
-  console.log (texto);
+
+  console.log ("longitug",contarLongitud(texto));
 }
 
 function mayusculas(txt)
 {
   return txt.toUpperCase();
+}
+
+
+function contarLongitud(){
+  let texto=document.querySelector("#texto").value ;
+  return texto.length
 }
