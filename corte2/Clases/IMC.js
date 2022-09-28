335669c21452665def450b15946b350351aafe07
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-27 20:08:44 -0500

    COMMIT

---
+++ corte2/Clases/IMC.js
@@ -0,0 +1,12 @@
+function convertir(){
+
+let peso= document.querySelector("#peso").value ;
+let altura= document.querySelector("#altura").value ;
+let resultado= (peso/(altura*altura));
+
+console.log(altura);
+console.log(peso);
+console.log(resultado);
+
+document.getElementById("mensaje").innerHTML=resultado;
+}
