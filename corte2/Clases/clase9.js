f57cbaab4d70a72722ef51945df0014cf82532e1
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-04 18:41:27 -0500

    yoyoyo

---
+++ corte2/Clases/clase9.js
@@ -0,0 +1,5 @@
+let animales= ["leon","salmon","mirla","gato","zorro","elefante","hipopotamo","flamenco","perro","delfi","mariposa","lobo","leon","tigre","pantera","camaleon","condor","aguila","oso","camello"];
+
+for (let i=0;i<animales.length;i++){
+    
+}
