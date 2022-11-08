27479fc69ba42abcb25d7ae6f81571eb463af81d
diego alejandro rodriguez calvo <diegoal63@hotmail.com>
2022-11-07 20:10:56 -0500

    usuario

---
+++ corte2/Proyecto/Adopcion.js
@@ -0,0 +1,18 @@
+
+export interface adopciones{
+    Nombre:string;
+    Tipo:string;
+    Raza:string;
+    Peso:Number;
+    Color:string;
+    Ciudad:string
+}
+
+let  adopciones:Adopciones = {
+    Nombre:"Nala";
+    Tipo:"Perro";
+    Raza:"Meztizo";
+    Peso:17;
+    Color:"Negro";
+    Ciudad:"Bogota"
+}
