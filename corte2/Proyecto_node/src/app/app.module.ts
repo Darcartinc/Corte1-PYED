3f5be01b0498e7a92d0f4a2708f7271383611a9a
diego alejandro rodriguez calvo <diegoal63@hotmail.com>
2022-11-20 22:53:09 -0500

    node 3

--- corte2/Proyecto_node/src/app/app.module.ts
+++ corte2/Proyecto_node/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { PerdidosComponent } from './components/perdidos/perdidos.component';
+import { AdoptarComponent } from './components/adoptar/adoptar.component';
+import { DonacionesComponent } from './components/donaciones/donaciones.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +16,10 @@ import { InicioComponent } from './components/inicio/inicio.component';
     HeaderComponent,
     FooterComponent,
     InicioComponent,
+    PerdidosComponent,
+    AdoptarComponent,
+    DonacionesComponent,
+    FormularioComponent,
   ],
   imports: [
     BrowserModule
