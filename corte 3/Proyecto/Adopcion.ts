91bb59e0d68194b0c49abcfaaff92d9e95e1a572
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-11-22 19:59:56 -0500

    final

---
+++ corte 3/Proyecto/Adopcion.ts
@@ -0,0 +1,99 @@
+
+export interface adopciones {
+    Nombre:string;
+    Tipo:string;
+    Raza:string;
+    Peso:Number;
+    Color:string;
+    Ciudad:string
+}
+
+let  adopciones1:adopciones = {
+    Nombre:"Nala",
+    Tipo:"Perro",
+    Raza:"Meztizo",
+    Peso:17,
+    Color:"Negro",
+    Ciudad:"Bogota"
+}
+let  adopciones2:adopciones = {
+    Nombre:"Jade",
+    Tipo:"Perro",
+    Raza:"Meztizo",
+    Peso:12,
+    Color:"Negro y beige ",
+    Ciudad:"Bogota"
+}
+let  adopciones3:adopciones = {
+    Nombre:"Ben",
+    Tipo:"Perro",
+    Raza:"Meztizo",
+    Peso:14,
+    Color:"Negro",
+    Ciudad:"Bogota"
+}
+let  adopciones4:adopciones = {
+    Nombre:"Sky",
+    Tipo:"Perro",
+    Raza:"Meztizo",
+    Peso:20,
+    Color:"Negro",
+    Ciudad:"Bogota"
+}
+let  adopciones5:adopciones = {
+    Nombre:"Florentina",
+    Tipo:"Gato",
+    Raza:"Meztiza",
+    Peso:8,
+    Color:"Negro y amarillo",
+    Ciudad:"Madrid"
+}
+let  adopciones6:adopciones = {
+    Nombre:"Alaska",
+    Tipo:"Perro",
+    Raza:"Golden retriever",
+    Peso:30,
+    Color:"Amarillo",
+    Ciudad:"Bogota"
+}
+let  adopciones7:adopciones = {
+    Nombre:"Bimba",
+    Tipo:"Perro",
+    Raza:"Meztiza",
+    Peso:16,
+    Color:"Negro y amarillo",
+    Ciudad:"Bogota"
+}
+let  adopciones8:adopciones = {
+    Nombre:"Kira",
+    Tipo:"Perro",
+    Raza:"Golden retriever",
+    Peso:30,
+    Color:"Amarillo",
+    Ciudad:"Bogota"
+}
+let  adopciones9:adopciones = {
+    Nombre:"Dante",
+    Tipo:"Perro",
+    Raza:"Dalmata",
+    Peso:21,
+    Color:"Blanco y negro",
+    Ciudad:"Bogota"
+}
+let  adopciones10:adopciones = {
+    Nombre:"Toy",
+    Tipo:"Gato",
+    Raza:"Siamese",
+    Peso:3,
+    Color:"Blanco y gris",
+    Ciudad:"Bogota"
+}
+let  adopciones11:adopciones = {
+    Nombre:"Nata",
+    Tipo:"Gato",
+    Raza:"Meztiza",
+    Peso:5,
+    Color:"Gris",
+    Ciudad:"Tabio"
+}
+
